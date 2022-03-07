96da9cd59d4869ecf5d4fb21cdc9f3a1f0ab55b2
zhaohw <zhaohw@segimail.com>
2022-03-07 17:23:55 +0800

    新增dashboard

---
+++ rocketmq/startDashboard.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo
+local_ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"|grep 192`
+echo "local ip is $local_ip"
+echo
+
+contaier_name="rocketmq-dashboard"
+
+# 下载容器镜像
+image_num=`docker image ls | grep "apacherocketmq/rocketmq-dashboard" | wc -l`
+if [ $image_num -eq 0 ]
+then
+    docker pull apacherocketmq/rocketmq-dashboard:latest
+fi
+
+# 启动容器
+container_num=`docker container ls -a | grep "apacherocketmq/rocketmq-dashboard" | wc -l`
+if [ $container_num -eq 0 ]
+then
+    echo "启动容器 ... ...."
+    docker run -d --name $contaier_name -e "JAVA_OPTS=-Drocketmq.namesrv.addr=$local_ip:9876" -p 8080:8080 -t apacherocketmq/rocketmq-dashboard:latest
+else
+    docker container start $contaier_name
+fi
+
+echo "访问地址: http://$local_ip:8080"
--- rocketmq/startRocketmq.sh
+++ rocketmq/startRocketmq.sh
@@ -5,16 +5,13 @@ echo
 rocketmq_path="/usr/local/programs/rocketmq-4.9.2"
 echo "rocketmq path is $rocketmq_path"
 
-local_ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"|grep 192`
-echo "local ip is $local_ip"
-
 #***************
 #
 # rocketmq默认使用的java版本是java8，如果要使用的java版本是java11，需要进行如下操作
 # 1. 下载java8运行环境，并配置环境变量$JAVA_HOME_8=/path/to/java8
 # 2. 修改rocketmq的配置文件：runserver.sh、runbroker.sh、tools.sh，修改运行所需要的配置，如内存大小等
 # 3. 修改2中的配置文件，将JAVA_HOME替换成JAVA_HOME_8: %s/JAVA_HOME/JAVA_HOME_8/g
-# 
+#
 # 注意：默认java8时，步骤2也是一定要改动的，因为rocketmq的配置需求要求太高
 #
 #***************
@@ -22,7 +19,7 @@ ehco
 
 # 启动namesrv
 nohup bash $rocketmq_path/bin/mqnamesrv &
-echo 
+echo
 # 启动borker
 nohup bash $rocketmq_path/bin/mqbroker -c conf/broker.conf -n 127.0.0.1:9876 &
 
